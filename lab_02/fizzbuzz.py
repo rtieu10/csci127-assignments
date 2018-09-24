e1a48da209e8ea48ca45c0a540a745b599d4703c
Rachel Tieu <rachel.tieu02@myhunter.cuny.edu>
2018-09-24 09:16:55 -0400

    fizzbuzz.py

---
+++ lab_02/fizzbuzz.py
@@ -0,0 +1,16 @@
+
+def fizzbuzz(max_value):
+    i=0
+    while i <= 99:
+        i = i + 1
+        if i % 3 == 0:
+            print ("Fizz")
+        elif i % 5 == 0: 
+            print ("Buzz")
+        elif i % 5 == 0 and i % 3 == 0: 
+            print ("FizzBuzz")
+       
+        else:
+            print (i)
+    
+fizzbuzz(100)
