ff94a6e0037b2a061b2bea1b1eff531d46623396
Rachel Tieu <rachel.tieu02@myhunter.cuny.edu>
2018-10-22 09:32:18 -0400

    exam_01.py

---
+++ exam_01/exam_01.py
@@ -0,0 +1,44 @@
+def divide(A,B,u):
+    cake = A/B
+    people = u / cake
+    return int(people)
+
+print(divide(5,10,1))
+print(divide(6,10,5))
+
+def compress_word(w):
+    l = ['a', 'e', 'i', 'o', 'u', 'A' , 'E', 'I' , 'O' , 'U']
+    i = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z', 'B','C','D',
+         'F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z']
+    for i in w:
+         s = w[0] + i
+    #s = [w[0], s.append(i)]
+    return(s)
+        
+print(compress_word("halloween"))
+print(compress_word("hello"))
+
+def compress_word(w):
+    l = ['a', 'e', 'i', 'o', 'u', 'A' , 'E', 'I' , 'O' , 'U']
+    i = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z', 'B','C','D',
+         'F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z']
+    for i in w:
+         s = w[0] + i  
+    #s = [w[0], s.append(i)]
+    return(s)
+
+print(compress_word("hello"))
+print(compress_word("people"))
+
+
+
+
+sentence = "Who is there"  
+def sentence(line):
+          for i in s.split():
+             sentence = 
+  
+       # return" ".join(sentence)
+        
+
+    #if i not in l:
