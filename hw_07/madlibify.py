3c0e59638c1cde2c8c92c5d4e3ada4c42e1ae757
Rachel Tieu <rachel.tieu02@myhunter.cuny.edu>
2018-11-07 22:59:01 -0500

    madlibify.py

---
+++ hw_07/madlibify.py
@@ -0,0 +1,59 @@
+import random
+dict = {}
+dict['NOUNS'] = ['dog','sandwich','cat','food','frog']
+dict['VERBS'] = ['ate','walked','ran','skipped']
+dict['HEROS'] = ['Spiderman','Hulk','Flash']
+dict['ADJECTIVES'] = ['fast','slow','tired']
+#NOUNS = ['dog','sandwich','cat','food','frog']
+#VERBS = ['ate','walked','ran','skipped']
+#HEROS = ['Spiderman','Hulk','Flash']
+#ADJECTIVES = ['fast','slow','tired']
+
+sentence = "<ADJECTIVES> <HERO> <VERB> in the <NOUNS> and then <HERO> <VERB> <NOUNS> later."
+
+#if 'nouns' in dict:       #to check if the key exists 
+ #   print("YES")
+
+
+
+#def madlib(s,d):
+    #new_list = []
+    #H_random = random.choice(HEROS)
+   # for n in dict['nouns']:
+      #  new_list.append(random.choice(dict['nouns']))
+    #for v in dict['verbs']:
+     #   new_list.append(random.choice(dict['verbs'])) 
+    #for h in dict['heros']: 
+      #  new_list.append(random.choice(dict['heros']))
+    #for a in dict['adjectives']:
+     #   new_list.append(random.choice(dict['adjectives']))
+        
+            
+    #return " ".join(new_list)
+
+#print(madlib(sentence,dict))
+
+
+
+def madlib(s,d):
+    new_list = []
+    H_random = random.choice(d['HEROS'])
+    for item in s.split():
+        if item == "<NOUNS>":
+            new_list.append(random.choice(dict['NOUNS']))
+        elif item == "<VERB>":
+            new_list.append(random.choice(dict['VERBS']))
+        elif item == "<HERO>":
+            new_list.append(H_random)
+        elif item == "<ADJECTIVES>":
+            if sentence.index("<ADJECTIVES>") == 0:
+                #new_list.append(random.choice(ADJECTIVES))
+                n = random.choice(dict['ADJECTIVES'])
+                n = n.capitalize()
+                new_list.append(n)
+        else:
+            new_list.append(item)
+            
+    return " ".join(new_list)
+
+print(madlib(sentence,dict))
--- lab_04/lady.py
+++ lab_04/lady.py
@@ -1,16 +1,23 @@
 def happyLadybugs(b):
-    for character in b:
+    for character in b:                                     #for each character in the string b 
         if character != "_" and b.count(character) == 1:
             return "NO"
         
         
         if b.count("_") == 0:
-            for i in range(1,len(b)-1):
+            for i in range(1,len(b)-1):                     #make it len(b)-1 because the last string wont have a character after it you can start at 1 and check the one before it or 0 and check the one after 
                 if b[i-1] != b[i] and b[i+1] != b[i]:
                     return "NO"
         
-     return "YES"
+    return "YES"
 
 print(happyLadybugs("ABCC_ACB"))
 print(happyLadybugs("AAB"))
 print(happyLadybugs("AABB"))
+
+#if theres a space you need at least 2 ladybugs of the same color
+#there needs to be at least 2 ladybugs of each color and at least one space
+#if there is a space then you only need to determine if theres at least 2 of every color
+#you can use what we need in the mode function to see how many o the letter there are
+#you can use the sorted command where you can sort it in a list and check the amount
+
