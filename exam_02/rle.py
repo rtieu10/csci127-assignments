54c80e2c6e2de264103bfc1f9289fae5c7aac9ad
Rachel Tieu <rachel.tieu02@myhunter.cuny.edu>
2018-11-19 12:02:15 -0500

    post exam solution

--- exam_02/rle.py
+++ exam_02/rle.py
@@ -1,4 +1,32 @@
-def encode(x):
+def encode(s):
+    l=[]
+    count=1
+    for i in range(len(s)-1):
+        if s[i]==s[i+1]:
+            count+=1
+        else:
+            x=[s[i],count]
+            l.append(x)
+            count=1
+    l.append([s[-1],count])
+    return l
+
+print(encode('aaabbbccaaaddeee'))
+x = encode('aaabbbccaaaddeee')
+
+
+def decode(l):
+    s = ""
+    for i in l:
+        for count in range(i[1]):
+            s+=i[0]
+    return s
+        
+            
+print(decode(x))
+
+
+"""def encode(x):
     dict = {}
     for letter in x:
         dict.setdefault(letter,0)
@@ -48,9 +76,4 @@ def decode(x):
         letter = elements[0]        
         number = elements[1]
         total = letter * number     #you can try to use .join to join the whole string together again
-        
-        
-    
-        
-    
-    
+"""
