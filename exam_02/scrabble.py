ea651d0bf66f35316c906ea5be6da7096c9b7c50
Rachel Tieu <rachel.tieu02@myhunter.cuny.edu>
2018-11-19 12:21:49 -0500

    README.md

--- exam_02/scrabble.py
+++ exam_02/scrabble.py
@@ -1,32 +1,4 @@
-<<<<<<< HEAD
-def score(x):
-    one = "AEIOULNRSTaeioulnrst"
-    two = "DGdg"
-    three = "BCMPbcmp"
-    four = "FHVWYfhvwy"
-    five = "Kk"
-    eight = "JXjx"
-    ten = "QZqz"
-    thescore = 0
-    for i in x: 
-        if i in one:
-            thescore += 1
-        elif i in two:
-            thescore += 2
-        elif i in three:
-            thescore += 3
-        elif i in four:
-            thescore += 4
-        elif i in five:
-            thescore += 5
-        elif i in eight:
-            thescore += 8
-        elif i in ten:
-            thescore += 10
-    return thescore
 
-print(score("HELLO"))
-=======
 def score(x): 
 one = "AEIOULNRSTaeioulnrst" 
 two = "DGdg" 
