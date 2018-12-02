840d2b91263a1fd12e7a249b4ccc2bdb2cfa40a5
Rachel Tieu <rachel.tieu02@myhunter.cuny.edu>
2018-12-02 15:18:24 -0500

    test.cpp

---
+++ cpp/test.cpp
@@ -0,0 +1,22 @@
+#include <iostream> 
+
+int main() 
+{ 
+   int i; 
+   int some_variable=10, anther_variable; 
+
+
+   i = 20; 
+
+   i = i + some_variable; 
+   d = i; 
+
+   i = i / 4.2; 
+   std::cout << "i =  " << i << "/n"; 
+
+   d = d / 4.2; 
+   std::cout << "d= " << d << "/n"; 
+  
+  
+   return 0; 
+} 
