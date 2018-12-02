52b984d5bd08fc40b144fc7ce2654d23dd617fe8
Rachel Tieu <rachel.tieu02@myhunter.cuny.edu>
2018-12-02 15:03:03 -0500

    count.cpp

---
+++ hw_09/count.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+
+int main() { 
+     // defining a 
+     int a = 1; 
+     
+     //while loop 
+     while ( a < 20 ) { 
+         std::cout << a << std::endl;
+         a++;
+     } 
+     return 0; 
+} 
+    
