11fff579fcad707d9e95099b2b5132716e06ea98
Rachel Tieu <rachel.tieu02@myhunter.cuny.edu>
2018-12-02 15:17:53 -0500

    if.cpp

---
+++ cpp/if.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+
+int main() 
+{ 
+  int i = 20; 
+
+  if (i > 20)
+    std::cout << "I is greater than 10\n" 
+  std::cout << "more stuff not in the if statement\n"; 
+
+  std::cout << "\n\n"; 
+  if (i < 10) 
+    std::cout << "i is less than 10\n" 
+  std::cout << "with a second statemnt\n" 
+} 
+
+
+
